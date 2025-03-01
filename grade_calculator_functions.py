31c588b059801174f37be7b214b83cc0566d0334
Ma.Christina Marbani <marbanimachristina_bsit@plmun.edu.ph>
2025-03-01 15:02:27 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -16,11 +16,11 @@ def calculate_grade(score: float) -> str:
 	
     if score >= 90:
         return 'A'
-    elif score >= 80:
+    if score >= 80:
         return 'B'
-    elif score >= 70:
+    if score >= 70:
         return 'C'
-    elif score >= 60:
+    if score >= 60:
         return 'D'
     else:
         return 'F'
